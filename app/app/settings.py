7fd32132b3760a2a3cbf2fa63bccdc1568be1a87
swap-stack <swapnilakavishwar@gmail.com>
2021-03-10 10:55:22 +0530

    Added endpoint for creating users

--- app/app/settings.py
+++ app/app/settings.py
@@ -38,9 +38,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'rest_framework.authtoken',
 
     # local
     'core',
+    'user',
 ]
 
 MIDDLEWARE = [
